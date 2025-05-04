3e1798ffc8b4663b6b3467c44cf650e7206b29f5
danipillado <daniel.pillado@opendeusto.es>
2025-05-04 18:57:12 +0200

    Aplicacion de Admin (modelo herramienta)

--- AppDjango/admin.py
+++ AppDjango/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Empleado, Proyecto, Tarea
+from .models import Empleado, Proyecto, Tarea, Herramienta
 # Register your models here.
 admin.site.register(Empleado)
 admin.site.register(Proyecto)
 admin.site.register(Tarea)
+admin.site.register(Herramienta)
